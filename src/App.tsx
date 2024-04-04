fffb8ab52c8c52bd1a4e1da937c58610b882ddeb
Jedikay <kadyns0604@gmail.com>
2024-04-04 01:39:57 -0500

    init setup: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { createSignal } from "solid-js";
 import solidLogo from "./assets/solid.svg";
 import viteLogo from "/vite.svg";
+import Header from "./assets/components/Header";
 
 function App() {
   const [count, setCount] = createSignal(0);
 
   return (
     <>
+      <Header />
+
       <div class="bg-green-950">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="h-1 w-1" alt="Vite logo" />
+          <img src={viteLogo} class="h-100 w-100" alt="Vite logo" />
         </a>
         <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="h-1 w-1" alt="Solid logo" />
+          <img src={solidLogo} class="h-100 w-100" alt="Solid logo" />
         </a>
       </div>
       <h1>Mud & Moss</h1>
