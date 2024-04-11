768e63f2d1a9ee1bd2c4df97de1ca08252e68d77
Jedikay <kadyns0604@gmail.com>
2024-04-11 11:03:23 -0500

    fix:removed border

--- src/assets/components/Header/index.tsx
+++ src/assets/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <p class="bg-green-950 text-yellow-600 border-black">Mud & Moss</p>
+      <p class="bg-green-950 text-yellow-600">Mud & Moss</p>
     </>
   );
 }
