57d71ac95a478b9d4ba37792e1014d95b94f0d7f
Jedikay <kadyns0604@gmail.com>
2024-04-23 14:47:39 -0500

    feat:footer init

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,22 @@
 export default function Footer() {
   return (
-    <>
-      <div>footer</div>
-    </>
+    <footer class="flex items-center text-yellow-600 bg-green-950 h-34 w-screen">
+      <div class="flex flex-col items-center justify-between gap-4 py-4"></div>
+      <picture>
+        <img src="/public/images/Mud&Moss.jpg" class="w-12 rounded-full" />
+      </picture>
+      <p class="text-xs text-yellow-600">
+        <a
+          class="font-bold hover:underline hover:text-yellow-600"
+          href="https://greatidea.dev/"
+          target="_blank"
+        >
+          GreatIdea.dev
+        </a>
+      </p>
+      <p class="text-xs text-yellow-600">
+        &copy; 2024 Great Idea Development | All rights reserved.
+      </p>
+    </footer>
   );
 }
