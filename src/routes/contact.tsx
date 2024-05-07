33be347799e55017e5f269229579934f541c24f1
Jedikay <kadyns0604@gmail.com>
2024-05-06 19:25:14 -0500

    chore:renamed contact component

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,10 +1,7 @@
-import Contact from "~/components/Contact";
-
 export default function Contact() {
   return (
     <>
       <div>Import your components here like the index page</div>
-      <Contact />
     </>
   );
 }
