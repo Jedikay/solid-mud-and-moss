40ec4a6835ef42f59cbdb8b544d9c3b9aadb3fbd
Jedikay <kadyns0604@gmail.com>
2024-05-07 18:35:34 -0500

    chore:added filler data to products page

--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -1,9 +1,40 @@
 import ProductsModule from "~/components/ProductsModule";
 
+const products = [
+  {
+    name: "Product 1",
+    href: "#",
+    imageSrc: "your/picture/goes/here",
+    imageAlt: "This is product 1, a pretty vase.",
+    price: "$50",
+  },
+  {
+    name: "Product 2",
+    href: "#",
+    imageSrc: "your/picture/goes/here",
+    imageAlt: "This is product 2, a tasteful sculpture.",
+    price: "$75",
+  },
+  {
+    name: "Product 3",
+    href: "#",
+    imageSrc: "your/picture/goes/here",
+    imageAlt: "This is product 3, a jewelry holder.",
+    price: "$25",
+  },
+  {
+    name: "Product 4",
+    href: "#",
+    imageSrc: "your/picture/goes/here",
+    imageAlt: "This is product 4, a pitcher",
+    price: "$60",
+  },
+];
+
 export default function Products() {
   return (
     <>
-      <div>Import components here like the index page.</div>
+      <ProductsModule products={products} />
     </>
   );
 }
