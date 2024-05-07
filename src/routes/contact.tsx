aa758d5904e7a8bcd93845efc1011f04e2fcdb42
Jedikay <kadyns0604@gmail.com>
2024-05-06 19:28:15 -0500

    chore:fixed import

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,7 +1,10 @@
+import ContactForm from "~/components/ContactForm";
+
 export default function Contact() {
   return (
     <>
       <div>Import your components here like the index page</div>
+      <ContactForm />
     </>
   );
 }
