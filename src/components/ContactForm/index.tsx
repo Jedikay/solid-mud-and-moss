1594ee81f59c0af1dfc21bcda99a498f63fd61a5
Jedikay <kadyns0604@gmail.com>
2024-05-07 21:24:54 -0500

    chore:added image and styled contact page

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -5,8 +5,17 @@ export default function Contact() {
       class="flex flex-col items-center justify-center w-full gap-8 my-10 lg:items-start lg:flex-row"
     >
       <div class="w-full max-w-lg lg:w-1/2">
-        <h5 class="my-2 text-3xl font-bold text-green-700">Contact</h5>
-        <p class="max-w-md mb-4 text-yellow-600"> Your text here</p>
+        <h5 class="my-2 text-3xl font-bold text-green-700">
+          Want to create a custom piece for your home?
+        </h5>
+        <p class="text-2xl max-w-md mb-4 text-zinc-800">
+          {" "}
+          Reach out to us today!
+        </p>
+        <img
+          src="/public/images/earl-wilcox-7ZK_CuHroq4-unsplash.jpg"
+          class="rounded-lg"
+        />
         <div class="flex flex-row gap-2 socials">
           <a
             href="/"
@@ -16,14 +25,17 @@ export default function Contact() {
           <a
             href="/"
             target="_blank"
-            class="w-12 cursor-pointer text-slate-200 hover:text-green-700"
+            class="w-12 cursor-pointer text-yellow-700 hover:text-green-700"
           ></a>
         </div>
       </div>
       <div class="w-full max-w-md lg:flex-1">
         <form class="flex flex-col">
           <div class="mb-6">
-            <label id="name" class="block mb-2 text-sm font-medium text-black">
+            <label
+              id="name"
+              class="block mb-2 text-sm font-medium text-green-700"
+            >
               Full Name:
             </label>
             <input
@@ -31,12 +43,15 @@ export default function Contact() {
               type="text"
               id="name"
               required
-              class="bg-zinc-900 border border-zinc-700 placeholder-zinc-400 text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              class="border border-green-700 placeholder-zinc-500 text-zinc-800 text-sm rounded-lg block w-full p-2.5"
               placeholder="Jane Doe"
             />
           </div>
           <div class="mb-6">
-            <label id="email" class="block mb-2 text-sm font-medium text-white">
+            <label
+              id="email"
+              class="block mb-2 text-sm font-medium text-green-700"
+            >
               Email Address:
             </label>
             <input
@@ -44,14 +59,14 @@ export default function Contact() {
               type="email"
               id="email"
               required
-              class="bg-zinc-900 border border-zinc-700 placeholder-zinc-400 text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              class="border border-green-700 placeholder-zinc-500 text-zinc-800 text-sm rounded-lg block w-full p-2.5"
               placeholder="you@example.com"
             />
           </div>
           <div class="mb-6">
             <label
               id="subject"
-              class="block mb-2 text-sm font-medium text-white"
+              class="block mb-2 text-sm font-medium text-green-700"
             >
               Subject:
             </label>
@@ -60,27 +75,27 @@ export default function Contact() {
               type="text"
               id="subject"
               required
-              class="bg-zinc-900 border border-zinc-700 placeholder-zinc-400 text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              class="border border-green-700 placeholder-zinc-500 text-zinc-800 text-sm rounded-lg block w-full p-2.5"
               placeholder="Just saying hello"
             />
           </div>
           <div class="mb-6">
             <label
               id="message"
-              class="block mb-2 text-sm font-medium text-white"
+              class="block mb-2 text-sm font-medium text-green-700"
             >
               Message:
             </label>
             <textarea
               name="message"
               id="message"
-              class="bg-zinc-900 border border-zinc-700 placeholder-zinc-400 text-gray-100 text-sm rounded-lg block w-full p-2.5 min-h-28"
+              class=" border border-green-700 placeholder-zinc-500 text-zinc-800 text-sm rounded-lg block w-full p-2.5 min-h-28"
               placeholder="Let's talk about..."
             />
           </div>
           <button
             type="submit"
-            class="bg-primary-500 hover:bg-primary-600 text-black font-medium py-2.5 px-5 rounded-lg w-full border border-green-700 bg-neutral-300"
+            class="bg-green-700 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full border border-black"
           >
             Send Message
           </button>
